5b1042cddf65999a785840821bba360eb5499b2b
minhtrung3920 <trung392dg@gmail.com>
2019-09-24 15:31:08 +0700

    draw

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,52 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("helloo");
+
+            Hline(5, 'b'); Console.WriteLine();
+
+            Hline(6, 'a'); Console.WriteLine();
+            Dline(10, 'g'); Console.WriteLine();
+            
         }
+        static void Hline(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(c);
+        }
+        static void Dline(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+                Hline(i, '*');
+            Console.Write(c);
+        }
+
+        //fir line : n- 1 spaces, 1 star
+        // sec line: n-2 spaces, 1 star,  1 star, 1 star
+        //third line: n-3 spaces, 1 star, 3 spaces, 1 star
+        // four line: n-4 spaces, 1star, 4 space, 1 star
+        //last: 2n-1 spaces stars
+
+
+        static void DrawTriangle(int n, char c) 
+    {
+        //first line
+        Hline(n - 1, ' '); 
+        Hline(1, c );
+        Console.WriteLine();
+
+            int a = 1; int b = 2;
+            Hline(n - a,' ');
+            Hline(n - b,' ');
+        
+        for (int i = 0; i < n; i++)
+
+
+
+        Hline(2 * n - 1, '*');
+        Console.WriteLine();
+    }
+
     }
 }
